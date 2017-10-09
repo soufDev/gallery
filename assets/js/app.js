f870ba8e6411f58c39ac361b4770bc010d82f3f7
soufDev <ait.akkache.soufiane@gmail.com>
2017-10-09 02:10:48 +0100

    code refactoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ let initPage = `
 
 let container = document.body
 container.innerHTML = initPage
+
 let index = 0
 let imageCount = 5
 let pictureURl = "http://lorempixel.com/600/350/?"
@@ -31,64 +32,99 @@ let refresh = function(prev, next) {
     }, 600)
 }
 
-document.querySelector('input[type="submit"]').addEventListener('click', (event) =>  {
-    event.preventDefault()
+let createGalleryWrapperElement = function (){
+    let galleryWrapper = document.createElement('div')
+    galleryWrapper.id = 'gallerywrapper'
+    return galleryWrapper
+}
 
-    imageCount = parseInt(document.getElementById('pictureNumber').value, 10)
+let creationGalleryElement = function () {
+    let gallery = document.createElement('div')
+    gallery.id = 'gallery'
+    return gallery
+}
 
-    if(!isNaN(imageCount)) {
-        let galleryWrapper = document.createElement('div')
-        galleryWrapper.id = 'gallerywrapper'
-
-        let gallery = document.createElement('div')
-        gallery.id = 'gallery'
-
-        galleryWrapper.appendChild(gallery)
-        container.innerHTML = galleryWrapper.outerHTML
-
-        let div = document.getElementById("gallery")
-
-        let prev = document.createElement('button')
-        prev.className = 'previous'
-        prev.innerHTML = 'Prev'
-        prev.addEventListener('click', (event) => {
-            event.preventDefault()
-            let old = index
-            index = (index-1+imageCount) % imageCount
-            refresh(old, index)
-        })
-
-
-        let next = document.createElement('button')
-        next.className = 'next'
-        next.innerHTML = 'Next'
-        next.addEventListener('click', (event) =>  {
-            event.preventDefault()
-            let old = index
-            index = (++index) % imageCount
-            refresh(old, index)
-        })
-
-        title = document.createElement('h3')
-
-        let preview = document.createElement('div')
-        preview.id= "preview"
-        for(let i=1; i<=imageCount; i++) {
-            let image = document.createElement('img')
-            image.id = i
-            image.src = pictureURl+i
-            image.className = 'fadeOut'
-            image.style.display = 'none'
-            imageList.push(image)
-            preview.appendChild(image)
-        }
-        div.appendChild(preview)
-        div.appendChild(prev)
-        div.appendChild(next)
-        div.appendChild(title)
-
-        refresh(0,0)
+let createImageList= () => {
+    let preview = createPreviewElement()
+    for(let i=1; i<=imageCount; i++) {
+        let image = document.createElement('img')
+        image.id = i
+        image.src = pictureURl+i
+        image.className = 'fadeOut'
+        image.style.display = 'none'
+        imageList.push(image)
+        preview.appendChild(image)
     }
+    return preview
+}
 
-})
+let nextImage = (event) =>  {
+    event.preventDefault()
+    let old = index
+    index = (++index) % imageCount
+    refresh(old, index)
+}
+
+let prevImage = (event) => {
+    event.preventDefault()
+    let old = index
+    index = (index-1+imageCount) % imageCount
+    refresh(old, index)
+}
+
+let creationNextImageButton = () => {
+    let next = document.createElement('button')
+    next.className = 'next'
+    next.innerHTML = 'Next'
+    next.addEventListener('click', nextImage)
+    return next
+}
+
+let creationPrevImageButton = () => {
+    let prev = document.createElement('button')
+    prev.className = 'previous'
+    prev.innerHTML = 'Prev'
+    prev.addEventListener('click', prevImage)
+    return prev
+}
+
+let creationTitleElement = () => {
+    return document.createElement('h3')
+}
+
+let createPreviewElement = () => {
+    let preview = document.createElement('div')
+    preview.id= "preview"
+    return preview
+}
+
+let setUpGalleryInContainer = () => {
+    let galleryWrapper = createGalleryWrapperElement()
+    let gallery = creationGalleryElement()
+    galleryWrapper.appendChild(gallery)
+    container.innerHTML = galleryWrapper.outerHTML
+}
+
+let setUI = () => {
+    setUpGalleryInContainer()
+    let gallery = document.getElementById("gallery")
+    let preview = createImageList()
+    let prev = creationPrevImageButton()
+    let next = creationNextImageButton()
+    title = creationTitleElement()
+    gallery.appendChild(preview)
+    gallery.appendChild(prev)
+    gallery.appendChild(next)
+    gallery.appendChild(title)
+    refresh(0,0)
+}
+
+let openGallery = (event) =>  {
+    event.preventDefault()
+    imageCount = parseInt(document.getElementById('pictureNumber').value, 10)
+    if(!isNaN(imageCount)) {
+        setUI()
+    }
+}
+document.querySelector('input[type="submit"]').addEventListener('click', openGallery)
 
